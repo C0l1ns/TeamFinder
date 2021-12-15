ff9918bb5964bbcbda5be0c0818f0fcf584a17df
Maksym-Leshchuk <maksym.leshchuk.knm.2020@lpnu.ua>
2021-12-15 19:05:25 +0200

    a

---
+++ TeamFinderDAL/Models/Entities/Tag.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace TeamFinderDAL.Models.Entities
+{
+    public class Tag
+    {
+        public int TagId { get; set; }
+        
+        public string Name { get; set; }
+
+        public IList<BoardGame> BoardGames { get; set; }
+    }
+}
---
+++ TeamFinderDAL/Models/EntityConfigurations/TagConfiguration.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using TeamFinderDAL.Models.Entities;
+
+namespace TeamFinder.Models.EntityConfigurations
+{
+    public class TagConfiguration : IEntityTypeConfiguration<Tag>
+    {
+        public void Configure(EntityTypeBuilder<Tag> builder)
+        {
+            builder.HasKey(t => t.TagId);
+
+            builder.Property(t => t.Name);
+        }
+    }
+}
